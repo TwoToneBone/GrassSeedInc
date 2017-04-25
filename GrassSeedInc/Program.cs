0fe9d09b305eb9a56bdd077b352ca4e7cac028d7
Erik <erik.ra@gmail.com>
2017-04-25 17:13:41 +0200

    Add project files.

---
+++ GrassSeedInc/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrassSeedInc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal output = 0;
+            decimal c = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            int l = int.Parse(Console.ReadLine());
+
+            string[] inputs = new string[l];
+
+            for (int i = 0; i < l; i++)
+            {
+                inputs[i] = Console.ReadLine();
+            }
+
+            foreach (var s in inputs)
+            {
+                string[] widthAndHeight = s.Split(' ');
+                                
+                output += (decimal.Parse(widthAndHeight[0], CultureInfo.InvariantCulture) * decimal.Parse(widthAndHeight[1], CultureInfo.InvariantCulture)) * c;
+            }
+
+            Console.WriteLine(output.ToString("0.0000000",CultureInfo.InvariantCulture));
+        }
+    }
+}
